c4a8b19108f6cb4cb98cb2836b1efbf37e7de162
Szabó Roland <rolandszb111@gmail.com>
2023-12-11 20:10:06 +0100

    meth7

--- meth7.py
+++ meth7.py
@@ -34,7 +34,7 @@ def Beolvas():
         i=0
         for item in f:
             if i >= 1:
-                i+=1
+                i += 1
                 tomb.append(RenSzarvas(item))
             else:
                 i+=1
@@ -44,15 +44,50 @@ def Beolvas():
 def main():
     print("Ez a program a Mikulasszan.txt szövegesfájlból beolvassa és kiértékeli az adatokat.")
     tomb = Beolvas()
-    print("Rénszarvasok neve, magassága")
+    # a. feladat:
+    print("Rénszarvasok neve, magassága:")
     for item in tomb:
-        print(f"{item.nev[0]}, {item.magassag}")
+        print(f"\t{item.nev[0]}: {item.magassag} cm")
+    # b. feladat:
     print(f"Rénszarvasok száma: {len(tomb)}")
+    # c. feladat:
     for item in tomb:
         if item.nev[1] != None and item.nev[1] == "Pompás":
-            print(item.nev[0])
-
-
+            print(f"\t{item.nev[0]}")
+    # d. feladat:
+    print("Szavak előfordulása a leírásokban: :")
+    for item in tomb:
+        print(f"\t{item.nev[0]}: {len(item.leiras.lower().split('mikulás'))-1}")
+    # e. feladat:
+    atlag = 0
+    osszeg = 0
+    darab = 0
+    for item in tomb:
+        if item.magassag > 0:
+            osszeg += item.magassag
+            darab += 1
+    print(f"A rénszarvasok átlagos magassága: {float(osszeg)/float(darab)}")
+    # f. feladat:
+    print("Páros helyen lévő rénszarvasok neve: ")
+    for item in tomb:
+        if item.hely % 2 == 0:
+            print(f"\t{item.nev[0]}")
+    # g. feladat:
+    maxHossz = 0
+    nev=""
+    for item in tomb:
+        if len(item.getLeiras()) > maxHossz:
+            maxHossz = len(item.getLeiras())
+            nev = item.nev[0]
+    print(f"Leghosszabb leírás: {nev}")
+    # h. feladat:
+    minHely = tomb[0].hely
+    nev = ""
+    for item in tomb:
+        if minHely >= item.hely:
+            minHely = item.hely
+            nev = item.nev[0]
+    print(f"Legkisebb számú hely: {nev}")
 
 if __name__ == '__main__':
     main()
